0cb6e62b934705692fac49dc21c98d373cb2efb3
Sergey Zelenov <github@zelenov.su>
2023-01-19 19:21:40 +0100

    add webhook

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const crypto = require('node:crypto');
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -6,7 +7,23 @@ bot.start((ctx) => ctx.reply('привет'));
 
 bot.on('message', (ctx) => ctx.reply('привет'));
 
-bot.launch();
+bot.launch({
+    webhook: {
+        // Public domain for webhook; e.g.: example.com
+        domain: process.env.PUBLIC_DOMAIN,
+
+        // Port to listen on; e.g.: 8080
+        port: process.env.PORT,
+
+        // Optional path to listen for.
+        // `bot.secretPathComponent()` will be used by default
+        // hookPath: webhookPath,
+
+        // Optional secret to be sent back in a header for security.
+        // e.g.: `crypto.randomBytes(64).toString("hex")`
+        secretToken: crypto.randomBytes(64).toString("hex")
+    },
+});
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
